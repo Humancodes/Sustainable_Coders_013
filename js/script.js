9245cc8a7c23099367c478a8ea7f95cbc8d8b847
CoderRoyshub <royshubham2401@gmail.com>
2024-10-02 22:10:57 +0530

    footer added

---
+++ js/script.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primegreen: '#43c370',
+                hovergreen: '#009467',
+                bgfooter: '#141a26',
+            }
+        },
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
+    }
+}
